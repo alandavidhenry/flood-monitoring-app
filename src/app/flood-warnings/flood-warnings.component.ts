2dc90499b5d62b757efc761e9b5d8693a607fa68
Alan Henry <alandavidhenry@gmail.com>
2024-10-15 09:22:49 +0100

    feat: add table sorting

--- src/app/flood-warnings/flood-warnings.component.ts
+++ src/app/flood-warnings/flood-warnings.component.ts
@@ -1,47 +1,83 @@
 import { Component, OnInit } from '@angular/core'
 import { Subscription } from 'rxjs'
 import { FloodApiService } from '../services/flood-api.service'
+import { Item } from '../models/flood'
 
 @Component({
   selector: 'app-flood-warnings',
   templateUrl: './flood-warnings.component.html',
   styleUrls: ['./flood-warnings.component.scss']
 })
-export class FloodWarningsComponent {
-  // Properties
-  floodWarningData: any = {}
+export class FloodWarningsComponent implements OnInit {
+  floodWarningData: Item[] = []
   private subscription!: Subscription
+  sortColumn: string = ''
+  sortDirection: 'asc' | 'desc' = 'asc'
 
-  // Inject service into constructor
   constructor(private floodApiService: FloodApiService) {}
 
   ngOnInit(): void {
-    // Subscribe to data
     this.subscription = this.floodApiService.getFloodWarningToday().subscribe({
-      next: (data) => {
-        this.floodWarningData = data // Save to property
+      next: (data: any) => {
+        this.floodWarningData = data.items
         console.log(data)
       },
       error: (error) => {
-        console.error(error) // Log an error if data cannot be retrieved
+        console.error(error)
       }
     })
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe() // Unsubscribe to prevent potential memory leaks
+    this.subscription.unsubscribe()
+  }
+
+  sortData(column: string): void {
+    if (this.sortColumn === column) {
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc'
+    } else {
+      this.sortColumn = column
+      this.sortDirection = 'asc'
+    }
+
+    this.floodWarningData.sort((a, b) => {
+      const valueA = this.getValue(a, column)
+      const valueB = this.getValue(b, column)
+
+      if (valueA < valueB) return this.sortDirection === 'asc' ? -1 : 1
+      if (valueA > valueB) return this.sortDirection === 'asc' ? 1 : -1
+      return 0
+    })
+  }
+
+  private getValue(item: Item, column: string): any {
+    switch (column) {
+      case 'location':
+        return item.description
+      case 'county':
+        return item.floodArea.county.split(',')[0].trim()
+      case 'severity':
+        return item.severityLevel
+      case 'time':
+        return new Date(item.timeRaised).getTime()
+      default:
+        return ''
+    }
   }
 
   sortByLocation(): void {
-    console.log('sort by location')
+    this.sortData('location')
   }
+
   sortByCounty(): void {
-    console.log('sort by county')
+    this.sortData('county')
   }
+
   sortBySeverity(): void {
-    console.log('sort by severity')
+    this.sortData('severity')
   }
+
   sortByTime(): void {
-    console.log('sort by time')
+    this.sortData('time')
   }
 }
